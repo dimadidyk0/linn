2f911629ebff2c6f3e063d1d6bd59e1409042275
dimadidyk0 <dimadidyk0@gmail.com>
2023-08-21 21:53:18 +0200

    feat: add route constants and pages handler

--- src/hooks/layout/useNavigation.js
+++ src/hooks/layout/useNavigation.js
@@ -1,7 +1,15 @@
+import {
+  BASE_ROUTE,
+  LEVELS_ROUTE,
+  PRACTICE_ROUTE,
+  THEORY_ROUTE,
+} from "../../constants/routeTemplates";
+
 export const useNavigation = () => {
   return [
-    { title: "Levels", link: "/levels" },
-    { title: "Theory", link: "/theory" },
-    { title: "Practive", link: "/practive" },
+    { title: "Home", link: BASE_ROUTE },
+    { title: "Levels", link: LEVELS_ROUTE },
+    { title: "Theory", link: THEORY_ROUTE },
+    { title: "Practive", link: PRACTICE_ROUTE },
   ];
 };
--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import reportWebVitals from "./reportWebVitals";
 import "./assets/styles/index.css";
 import "./assets/styles/variables.css";
 import { Layout } from "./components/ui/Layout/Layout";
+import {
+  BASE_ROUTE,
+  LEVELS_ROUTE,
+  PRACTICE_ROUTE,
+  THEORY_ROUTE,
+} from "./constants/routeTemplates";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,7 +20,10 @@ root.render(
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<App />} />
+          <Route path={BASE_ROUTE} element={<App />} />
+          <Route path={LEVELS_ROUTE} element={<App />} />
+          <Route path={THEORY_ROUTE} element={<App />} />
+          <Route path={PRACTICE_ROUTE} element={<App />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </Layout>
