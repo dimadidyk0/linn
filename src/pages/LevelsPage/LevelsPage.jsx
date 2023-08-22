6837b4f40149f6480e1e161273b2f9bdbe435f8b
dimadidyk0 <dimadidyk0@gmail.com>
2023-08-22 20:51:22 +0200

    chore: add title to levels page

--- src/pages/LevelsPage/LevelsPage.jsx
+++ src/pages/LevelsPage/LevelsPage.jsx
@@ -1,12 +1,14 @@
 import LevelBanner from "../../components/LevelBanner/LevelBanner";
 import Container from "../../components/ui/Container/Container";
 import { useFetchLevels } from "../../hooks/api/useFetchLevels";
+import s from "./LevelsPage.module.css";
 
 export function LevelsPage() {
   const { data, isLoading } = useFetchLevels();
 
   return (
     <Container>
+      <h1 className={s.title}>Levels</h1>
       {isLoading
         ? "Loading..."
         : data.map((level) => (
