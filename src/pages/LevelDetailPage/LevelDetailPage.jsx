644a4e2ec2990adb42fbe7d7977eb48b57a9a11b
dimadidyk0 <dimadidyk0@gmail.com>
2023-08-23 21:48:52 +0200

    refactor: connect detail page with router

--- src/pages/LevelDetailPage/LevelDetailPage.jsx
+++ src/pages/LevelDetailPage/LevelDetailPage.jsx
@@ -1,22 +1,15 @@
-// import PropTypes from "prop-types";
+import { useParams } from "react-router-dom";
 import Container from "../../components/ui/Container/Container";
 import s from "./LevelDetailPage.module.css";
 
 export function LevelDetailPage() {
-  // const { data, isLoading } = useFetchLevelDetail();
+  const { id } = useParams();
 
   return (
     <Container>
-      <h1 className={s.title}>Level ID</h1>
-      {/* {isLoading
-        ? "Loading..."
-        : data.map((level) => (
-            <LevelBanner key={level.id} {...level} />
-          ))} */}
+      <h1 className={s.title}>Level {id}</h1>
     </Container>
   );
 }
-
-LevelDetailPage.propTypes = {};
 
 export default LevelDetailPage;
