7ca18bbfd7c88fbd837d7640b0d88158eb51156a
dimadidyk0 <dimadidyk0@gmail.com>
2023-09-07 22:31:53 +0200

    chore: remove useless code

--- src/pages/PractivePage/PractivePage.jsx
+++
@@ -1,7 +0,0 @@
-import Container from "../../components/ui/Container/Container";
-
-export function PractivePage() {
-  return <Container>Theory</Container>;
-}
-
-export default PractivePage;
--- src/pages/TheoryPage/TheoryPage.jsx
+++
@@ -1,7 +0,0 @@
-import Container from "../../components/ui/Container/Container";
-
-export function TheoryPage() {
-  return <Container>Theory</Container>;
-}
-
-export default TheoryPage;
