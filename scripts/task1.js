44066505b6cbad97631af833bd093996a2bd7af6
dimadidyk0 <dimadidyk0@gmail.com>
2023-09-19 20:08:15 +0200

    chore: refactor task1

--- scripts/task1.js
+++ scripts/task1.js
@@ -1,11 +1,10 @@
 function solution(array) {
   try {
-    const binaryArray = array.reduce(
-      (acc, decimal) => [decimalToEightBinaryStr(decimal), ...acc],
-      []
+    const binaryNumberStr = array.reduce(
+      (acc, decimal) => `${decimalToEightBinaryStr(decimal)}${acc}`,
+      ""
     );
-
-    return parseInt(binaryArray.join(""), 2);
+    return parseInt(binaryNumberStr, 2);
   } catch (err) {
     console.log(`Error during executing '${solution.name}'`, err);
   }
@@ -13,15 +12,14 @@ function solution(array) {
 
 console.log(solution([24, 85, 0])); // 21784
 
-function decToBin(dec) {
-  return (dec >>> 0).toString(2);
-}
-
 function decimalToEightBinaryStr(decimal) {
   const MAX_BIT_AMIUNT = 8;
-  const numberAsAString = String(decToBin(decimal));
+  const numberAsAString = String(decimalToBinary(decimal));
+  const prefix = "0".repeat(MAX_BIT_AMIUNT - numberAsAString.length); // value "101" => will have "00000" as prefix
 
-  return `${"0".repeat(
-    MAX_BIT_AMIUNT - numberAsAString.length
-  )}${numberAsAString}`.slice(MAX_BIT_AMIUNT * -1);
+  return `${prefix}${numberAsAString}`.slice(MAX_BIT_AMIUNT * -1); // shorter versions insted of if/else statements
+}
+
+function decimalToBinary(dec) {
+  return (dec >>> 0).toString(2);
 }
