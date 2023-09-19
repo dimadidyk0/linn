c39963dd0f22916ac8891c1f3bd45cacee81fa22
dimadidyk0 <dimadidyk0@gmail.com>
2023-09-19 20:40:32 +0200

    feat: refactor task 2

--- scripts/task2.js
+++ scripts/task2.js
@@ -1,18 +1,41 @@
-function solution(array) {
-  const allNumbers = array.join("").split("");
-  const numbersMap = allNumbers.reduce((acc, num) => {
-    return {
-      ...acc,
-      [num]: acc[num] ? acc[num] + 1 : 1,
-    };
-  }, {});
-  const mostPopularNumberAmount = Math.max(
-    ...Object.values(numbersMap)
-  );
+// function solution(array) {
+//   const allNumbers = array.join("").split("");
+//   const numbersMap = allNumbers.reduce((acc, num) => {
+//     return {
+//       ...acc,
+//       [num]: acc[num] ? acc[num] + 1 : 1,
+//     };
+//   }, {});
+//   const mostPopularNumberAmount = Math.max(
+//     ...Object.values(numbersMap)
+//   );
 
-  return Object.entries(numbersMap)
-    .filter(([num, amount]) => amount === mostPopularNumberAmount)
-    .map(([num]) => Number(num));
+//   return Object.entries(numbersMap)
+//     .filter(([num, amount]) => amount === mostPopularNumberAmount)
+//     .map(([num]) => Number(num));
+// }
+
+function solution(array) {
+  const allNumbers = array.join("");
+  const map = {};
+  let maxAmount = 0;
+  const result = [];
+
+  for (let i = 0; i < allNumbers.length; i++) {
+    const key = allNumbers[i];
+    map[key] = map[key] ? map[key] + 1 : 1;
+
+    if (maxAmount < map[key]) maxAmount = map[key];
+  }
+
+  for (const property in map) {
+    const amount = map[property];
+    if (amount !== maxAmount) continue;
+
+    result.push(Number(property));
+  }
+
+  return result;
 }
 
 console.log(solution([1, 11, 1, 2])); // [1]
